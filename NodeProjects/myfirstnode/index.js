d63f15daf6b1f4b6c462d7795495a36273c3a553
Oaindriladas <oaindrila.das@hotmail.com>
2023-04-09 15:06:11 +0530

    save

--- NodeProjects/myfirstnode/index.js
+++ NodeProjects/myfirstnode/index.js
@@ -1,9 +1,14 @@
 var express= require('express');
 var app = express();
-
+app.listen(5050,()=>{ console.log("Server running!");})
 //get-select * from emp 
 //post-insert or write data into the database
-app.get('/home',(request, response)=>{
+app.get('/',(request, response)=>{
 response.write("Welcome home!");
+response.write("Halleliuiah");
 response.end();
-}).listen(5050,()=>{ console.log("Server running!");})
+})
+
+app.get('/aboutus',(request,response)=>{
+    response.send("<b>About us</b>");
+})
