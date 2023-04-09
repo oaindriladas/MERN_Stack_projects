202b2ea89fbde4216cbc7b7cac677956bdf13108
Oaindriladas <oaindrila.das@hotmail.com>
2023-04-09 15:53:51 +0530

    commit

--- NodeProjects/myfirstnode/index.js
+++ NodeProjects/myfirstnode/index.js
@@ -1,11 +1,14 @@
 var express= require('express');
 var app = express();
+var student= require('./student')
+
 app.listen(5050,()=>{ console.log("Server running!");})
 //get-select * from emp 
 //post-insert or write data into the database
 app.get('/',(request, response)=>{
-response.write("Welcome home!");
-response.write("Halleliuiah");
+// response.write("Welcome home!<br>");
+// response.write(student[0].sid.toString());
+response.json(student);
 response.end();
 })
 
---
+++ NodeProjects/myfirstnode/student.js
@@ -0,0 +1,12 @@
+var stud=[
+    {
+        sid:10,
+        sname:'Prashant'
+    },
+    {
+        sid:12,
+        sname:'Shruti'
+    }
+]
+
+module.exports=stud;
